637ed2b68c6d7e01b4620b60832b03cd8c0f2e44
kota-shiokara <kota.book.39@gmail.com>
2022-07-12 09:49:44 +0900

    fix: test and argument PGraphics

--- src/test/java/jp/ikanoshiokara/FPL/view/ColorTest.java
+++ src/test/java/jp/ikanoshiokara/FPL/view/ColorTest.java
@@ -13,41 +13,57 @@ public class ColorTest {
     @DisplayName("generateColor()")
     class GenerateColor {
         @Test
-        @DisplayName("Normal RGB in range test")
-        void generateColor_普通のRGB_範囲内() {
+        @DisplayName("Normal RGB in range test with PApplet")
+        void generateColor_普通のRGB_範囲内_PApplet() {
             // mock
             PApplet testApplet = new PApplet();
             testApplet.g = new PGraphics();
             testApplet.g.colorMode = PConstants.RGB;
+            testApplet.g.colorModeX = 255;
+            testApplet.g.colorModeY = 255;
+            testApplet.g.colorModeZ = 255;
 
             // run
             int result = ColorExt.generateColor(testApplet);
-            boolean red = 0 <= testApplet.red(result) && testApplet.red(result) <= testApplet.g.colorModeX;
-            boolean green = 0 <= testApplet.green(result) && testApplet.green(result) <= testApplet.g.colorModeY;
-            boolean blue = 0 <= testApplet.blue(result) && testApplet.blue(result) <= testApplet.g.colorModeZ;
-            boolean actual = red && blue && green;
+            float red = testApplet.red(result);
+            float green = testApplet.green(result);
+            float blue = testApplet.blue(result);
+
+            boolean isRed = 0 <= red && red <= testApplet.g.colorModeX;
+            boolean isGreen = 0 <= green && green <= testApplet.g.colorModeY;
+            boolean isBlue = 0 <= blue && blue <= testApplet.g.colorModeZ;
+            boolean actual = isRed && isGreen && isBlue;
 
             // assert
             assertTrue(actual);
         }
 
         @Test
-        @DisplayName("Normal RGB out of range test")
-        void generateColor_普通のRGB_範囲外() {
+        @DisplayName("Normal RGB in range test with PGraphics")
+        void generateColor_普通のRGB_範囲内_PGraphics() {
             // mock
+            PGraphics pg = new PGraphics();
+            pg.colorMode = PConstants.RGB;
+            pg.colorModeX = 255;
+            pg.colorModeY = 255;
+            pg.colorModeZ = 255;
+
             PApplet testApplet = new PApplet();
-            testApplet.g = new PGraphics();
-            testApplet.g.colorMode = PConstants.RGB;
+            testApplet.g = pg;
 
             // run
-            int result = ColorExt.generateColor(testApplet);
-            boolean red = 0 > testApplet.red(result) && testApplet.red(result) > testApplet.g.colorModeX;
-            boolean green = 0 > testApplet.green(result) && testApplet.green(result) > testApplet.g.colorModeY;
-            boolean blue = 0 > testApplet.blue(result) && testApplet.blue(result) > testApplet.g.colorModeZ;
-            boolean actual = red && blue && green;
+            int result = ColorExt.generateColor(pg);
+            float red = testApplet.red(result);
+            float green = testApplet.green(result);
+            float blue = testApplet.blue(result);
+
+            boolean isRed = 0 <= red && red <= pg.colorModeX;
+            boolean isGreen = 0 <= green && green <= pg.colorModeY;
+            boolean isBlue = 0 <= blue && blue <= pg.colorModeZ;
+            boolean actual = isRed && isGreen && isBlue;
 
             // assert
-            assertTrue(!actual);
+            assertTrue(actual);
         }
     }
 }
